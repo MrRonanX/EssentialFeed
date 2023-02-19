ae8a1d53e6f9a54a5a67856ef769569b2992416d
Roman Kavisnkyi <mrronanx@gmail.com>
2023-02-19 17:32:57 +0700

    Moved HTTPCLientProtocol to a separate file

---
+++ EssentialFeed/FeedAPI/HTTPClientProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClientProtocol.swift
+//  EssentialFeed
+//
+//  Created by Roman Kavinskyi on 2/19/23.
+//
+
+import Foundation
+
+public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
+public typealias ClientLoadResult = Result<[FeedItem], RemoteFeedLoader.Error>
+
+public protocol HTTPClient {
+    func get(from url: URL?, completion: @escaping (HTTPClientResult) -> Void)
+}
--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -7,12 +7,7 @@
 
 import Foundation
 
-public typealias HTTPClientResult = Result<(Data, HTTPURLResponse), Error>
-public typealias ClientLoadResult = Result<[FeedItem], RemoteFeedLoader.Error>
 
-public protocol HTTPClient {
-    func get(from url: URL?, completion: @escaping (HTTPClientResult) -> Void)
-}
 
 public final class RemoteFeedLoader {
     private let url: URL?
